91a53cd8ce4b1ccbaf66edbe1cc9c84c6c75a2e7
zeeshan714 <zeeshibhi786@gmail.com>
2026-08-03 00:23:29 +0500

    Added meta tag and script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Wordle Pro Game",
   description: "Play Wordle Pro and test your tech vocabulary skills!",
+  other: {
+    "google-adsense-account": "ca-pub-4451053574258511",
+  },
 };
 
 export default function RootLayout({
@@ -15,12 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4451053574258511"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        ></script>
       </head>
       <body>{children}</body>
     </html>
